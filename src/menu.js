d4a4e2cd28420b908c28a392a495e3a516edb753
ataliasloami <ataliasoliveira37@gmail.com>
2025-08-04 16:47:31 -0300

    Ajustado ao menu Bender

--- src/menu.js
+++ src/menu.js
@@ -14,121 +14,38 @@ exports.menuMessage = (groupJid) => {
   const prefix = getPrefix(groupJid);
 
   return `╭━━⪩ BEM VINDO! ⪨━━${readMore()}
-▢
-▢ • ${BOT_NAME}
-▢ • Data: ${date.toLocaleDateString("pt-br")}
-▢ • Hora: ${date.toLocaleTimeString("pt-br")}
-▢ • Prefixo: ${prefix}
-▢ • Versão: ${packageInfo.version}
-▢
-╰━━─「🪐」─━━
+┊
+┊ • ${BOT_NAME}
+┊ • Data: ${date.toLocaleDateString("pt-br")}
+┊ • Hora: ${date.toLocaleTimeString("pt-br")}
+┊ • Prefixo: ${prefix}
+┊ • Versão: ${packageInfo.version}
+┊
+╰─┈┈┈◜❁◞┈┈┈─╯
 
-╭━━⪩ DONO ⪨━━
-▢
-▢ • ${prefix}exec
-▢ • ${prefix}get-id
-▢ • ${prefix}off
-▢ • ${prefix}on
-▢ • ${prefix}set-menu-image
-▢ • ${prefix}set-prefix
-▢
-╰━━─「🌌」─━━
+╭┈❪⚙️ꕸ▸ *MENU PRINCIPAL*
+┊
+┊•.̇𖥨֗☢️⭟${prefix}s
+┊
+╰─┈┈┈◜❁◞┈┈┈─╯
 
 ╭━━⪩ ADMINS ⪨━━
-▢
-▢ • ${prefix}abrir
-▢ • ${prefix}add-auto-responder
-▢ • ${prefix}agendar-mensagem
-▢ • ${prefix}anti-audio (1/0)
-▢ • ${prefix}anti-document (1/0)
-▢ • ${prefix}anti-event (1/0)
-▢ • ${prefix}anti-image (1/0)
-▢ • ${prefix}anti-link (1/0)
-▢ • ${prefix}anti-product (1/0)
-▢ • ${prefix}anti-sticker (1/0)
-▢ • ${prefix}anti-video (1/0)
-▢ • ${prefix}auto-responder (1/0)
-▢ • ${prefix}ban
-▢ • ${prefix}delete
-▢ • ${prefix}delete-auto-responder
-▢ • ${prefix}exit (1/0)
-▢ • ${prefix}fechar
-▢ • ${prefix}hidetag
-▢ • ${prefix}limpar
-▢ • ${prefix}link-grupo
-▢ • ${prefix}list-auto-responder
-▢ • ${prefix}mute
-▢ • ${prefix}only-admin (1/0)
-▢ • ${prefix}promover
-▢ • ${prefix}rebaixar
-▢ • ${prefix}revelar
-▢ • ${prefix}unmute
-▢ • ${prefix}welcome (1/0)
-▢
-╰━━─「⭐」─━━
-
-╭━━⪩ PRINCIPAL ⪨━━
-▢
-▢ • ${prefix}attp
-▢ • ${prefix}cep
-▢ • ${prefix}exemplos-de-mensagens
-▢ • ${prefix}fake-chat
-▢ • ${prefix}gerar-link
-▢ • ${prefix}get-lid
-▢ • ${prefix}google-search
-▢ • ${prefix}perfil
-▢ • ${prefix}ping
-▢ • ${prefix}raw-message
-▢ • ${prefix}rename
-▢ • ${prefix}sticker
-▢ • ${prefix}to-image
-▢ • ${prefix}ttp
-▢ • ${prefix}yt-search
-▢
-╰━━─「🚀」─━━
-
-╭━━⪩ DOWNLOADS ⪨━━
-▢
-▢ • ${prefix}play-audio
-▢ • ${prefix}play-video
-▢ • ${prefix}tik-tok
-▢ • ${prefix}yt-mp3
-▢ • ${prefix}yt-mp4
-▢
-╰━━─「🎶」─━━
-
-╭━━⪩ BRINCADEIRAS ⪨━━
-▢
-▢ • ${prefix}abracar
-▢ • ${prefix}beijar
-▢ • ${prefix}dado
-▢ • ${prefix}jantar
-▢ • ${prefix}lutar
-▢ • ${prefix}matar
-▢ • ${prefix}socar
-▢
-╰━━─「🎡」─━━
-
-╭━━⪩ IA ⪨━━
-▢
-▢ • ${prefix}gemini
-▢ • ${prefix}ia-sticker
-▢ • ${prefix}pixart
-▢ • ${prefix}stable-diffusion-turbo
-▢
-╰━━─「🚀」─━━
-
-╭━━⪩ CANVAS ⪨━━
-▢
-▢ • ${prefix}blur
-▢ • ${prefix}bolsonaro
-▢ • ${prefix}cadeia
-▢ • ${prefix}contraste
-▢ • ${prefix}espelhar
-▢ • ${prefix}gray
-▢ • ${prefix}inverter
-▢ • ${prefix}pixel
-▢ • ${prefix}rip
-▢
-╰━━─「❇」─━━`;
+┊
+┊•.̇𖥨֗☢️⭟${prefix}anti-image (1/0)
+┊•.̇𖥨֗☢️⭟${prefix}anti-link (1/0)
+┊•.̇𖥨֗☢️⭟${prefix}anti-video (1/0)
+┊•.̇𖥨֗☢️⭟${prefix}ban
+┊•.̇𖥨֗☢️⭟${prefix}delete
+┊•.̇𖥨֗☢️⭟${prefix}fechar
+┊•.̇𖥨֗☢️⭟${prefix}hidetag
+┊•.̇𖥨֗☢️⭟${prefix}limpar
+┊•.̇𖥨֗☢️⭟${prefix}link-grupo
+┊•.̇𖥨֗☢️⭟${prefix}promover
+┊•.̇𖥨֗☢️⭟${prefix}rebaixar
+┊•.̇𖥨֗☢️⭟${prefix}revelar
+┊•.̇𖥨֗☢️⭟${prefix}mute
+┊•.̇𖥨֗☢️⭟${prefix}unmute
+┊•.̇𖥨֗☢️⭟${prefix}welcome (1/0)
+┊
+╰─┈┈┈◜❁◞┈┈┈─╯`;
 };
